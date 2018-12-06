b6fc1dea1dffd1d670ea15e7c73bbb4a48b507c4
Unknown <alejandromedinareyesmx@gmail.com>
2018-12-05 21:02:37 -0600

    some triggers and user management

--- database.sql
+++ database.sql
@@ -8,7 +8,7 @@ create table Country(CveCountry varchar(3) not null,
 create table Category (CveCat varchar(4) not null,
                        DescCat varchar(20),
 					   constraint CategoryPK primary key (CveCat));
-                       
+
 create table UserImage (ImageID smallint not null,
 						ImageDesc varchar(300) not null,
                         constraint UserImagePK primary key (ImageID));
@@ -20,8 +20,8 @@ create table UserB (UName varchar(15) not null,
 					UDate Date,
 					UType varchar(1),
 					UPictureID smallint,
-					constraint UserBPK primary key (Uname,Upassword),
-                    constraint UserFK1 foreign key (UPictureID) references UserImage (ImageID));
+					constraint UserBPK primary key (Uname),
+          constraint UserFK1 foreign key (UPictureID) references UserImage (ImageID));
 
 create table Book (Name varchar(120) not null,
 				   Review varchar(1000),
@@ -29,7 +29,7 @@ create table Book (Name varchar(120) not null,
 				   Cat varchar(4) not null,
 				   Calif float,
 				   Cover varchar(500),
-				   constraint BookPK primary key (Name, Link),
+				   constraint BookPK primary key (Name),
 				   constraint BookFK1 foreign key (Cat) references Category (CveCat));
 
 create table RBook (RName varchar(120) not null,
@@ -37,69 +37,103 @@ create table RBook (RName varchar(120) not null,
 				    RReview varchar(1000),
 					RCat varchar(4) not null,
 					RUname varchar(15) not null,
-                    RUpassword varchar(15) not null,
 					RCover varchar(500),
                     RCalif float,
-					constraint RBookPK primary key (RName,RLink),
+					constraint RBookPK primary key (RName),
 					constraint RBookFK1 foreign key (RCat) references Category(CveCat),
-					constraint RBookFK2 foreign key (RUname,RUpassword) references UserB(Uname,Upassword));
+					constraint RBookFK2 foreign key (RUname) references UserB(Uname));
 
 
 create table Favorites(FUName varchar(15) not null,
-                       FUPassword varchar(15) not null,
 					   FName varchar(120) not null,
-                       FLink varchar(500) not null,
-					   constraint FavoritesPK primary key (FUname,FUpassword,FName,FLink),
-					   constraint FavoritesFK1 foreign key (FUName,FUPassword) references UserB(UName,UPassword),
-					   constraint FavoritesFK2 foreign key (FName,FLink) references Book(Name,Link));
+					   constraint FavoritesPK primary key (FUname,FName),
+					   constraint FavoritesFK1 foreign key (FUName) references UserB(UName),
+					   constraint FavoritesFK2 foreign key (FName) references Book(Name));
 
 create table Ranking(GUName varchar(15) not null,
-                     GUPassword varchar(15) not null,
 					 GName varchar(120) not null,
-                     GLink varchar(500) not null,
                      GCalif smallint not null,
-					 constraint RankingPK primary key (GUname,GUpassword,GName,GLink),
-					 constraint RankingFK1 foreign key (GUName,GUPassword) references UserB(UName,UPassword),
-					 constraint RankingFK2 foreign key (GName,GLink) references Book(Name,Link));
+					 constraint RankingPK primary key (GUname,GName),
+					 constraint RankingFK1 foreign key (GUName) references UserB(UName),
+					 constraint RankingFK2 foreign key (GName) references Book(Name));
 
 
 create table History(HUName varchar(15) not null,
-                      HUPassword varchar(15) not null,
 					  HName varchar(120) not null,
-                      HLink varchar(500) not null,
 					  HDate Date,
-					  constraint HistoryPK primary key (HUname,HUpassword,HName,HLink),
-					  constraint HistoryFK1 foreign key (HUName,HUPassword) references UserB(UName,UPassword),
-					  constraint HistoryFK2 foreign key (HName,HLink) references Book(Name,Link));
-                      
-                      
+					  constraint HistoryPK primary key (HUname,HName),
+					  constraint HistoryFK1 foreign key (HUName) references UserB(UName),
+					  constraint HistoryFK2 foreign key (HName) references Book(Name));
+
+
 create table Comments(CUName varchar(15) not null,
-                      CUPassword varchar(15) not null,
 					  CName varchar(120) not null,
-                      CLink varchar(500) not null,
 					  COpinion varchar(200) not null,
-					  constraint CommentsPK primary key (CUName,CUPassword,CName,CLink,COpinion),
-					  constraint CommentsFK1 foreign key (CUName,CUPassword) references UserB(UName,UPassword),
-					  constraint CommentsFK2 foreign key (CName,CLink) references Book(Name,Link));
+					  constraint CommentsPK primary key (CUName,CName,COpinion),
+					  constraint CommentsFK1 foreign key (CUName) references UserB(UName),
+					  constraint CommentsFK2 foreign key (CName) references Book(Name));
 
 create table Autor(AName varchar(50) not null,
 				   ABirthDate date,
                    ACveCountry varchar(3) not null,
                    constraint AutorPK primary key (AName),
                    constraint AutorFK1 foreign key (ACveCountry) references Country(CveCountry));
-                   
-                   
 
 create table Owners(OName varchar (120) not null,
-			        OLink varchar (500) not null,
                     OAName varchar(50) not null,
-                    constraint OwnersPK primary key (OName,OLink,OAName),
-                    constraint OwnersFK1 foreign key (OName,OLink) references Book (Name,Link),
+                    constraint OwnersPK primary key (OName,OAName),
+                    constraint OwnersFK1 foreign key (OName) references Book (Name),
                     constraint OwnersFK2 foreign key (OAName) references Autor(AName));
 
 create table ROwners(ROName varchar (120) not null,
-			         ROLink varchar (500) not null,
                      ROAName varchar(50) not null,
-                     constraint ROwnersPK primary key (ROName,ROLink,ROAName),
-                     constraint ROwnersFK1 foreign key (ROName,ROLink) references RBook (RName,RLink));
-                     
+                     constraint ROwnersPK primary key (ROName,ROAName),
+                     constraint ROwnersFK1 foreign key (ROName) references RBook (RName));
+
+CREATE VIEW user_v AS SELECT * FROM UserB where UName = USER();
+
+CREATE USER 'guest@localhost' IDENTIFIED BY '123';
+GRANT SELECT ON Bookstore.* TO 'guest@localhost';
+GRANT INSERT, SELECT ON Bookstore.UserB TO 'guest@localhost';
+
+CREATE ROLE 'usuarios', 'administradores';
+GRANT ALL ON Bookstore.* TO 'administradores';
+GRANT SELECT ON Bookstore.* TO 'usuarios';
+GRANT INSERT ON Bookstore.RBook TO 'usuarios';
+GRANT INSERT ON Bookstore.Comments TO 'usuarios';
+GRANT INSERT ON Bookstore.History TO 'usuarios';
+GRANT INSERT, DELETE ON Bookstore.Favorites TO 'usuarios';
+GRANT INSERT, UPDATE ON Bookstore.Ranking TO 'usuarios';
+GRANT UPDATE ON Bookstore.user_v TO 'usuarios';
+
+CREATE TRIGGER create_user AFTER INSERT ON UserB
+FOR EACH ROW
+BEGIN
+	CREATE USER NEW.UName@'localhost' IDENTIFIED BY NEW.UPassword;
+	GRANT 'usuarios' TO NEW.UName@'localhost';
+END;//
+
+
+CREATE TRIGGER update_user_password AFTER UPDATE ON user_v
+FOR EACH ROW
+BEGIN
+	SET PASSWORD = NEW.UPassword;
+END;//
+
+
+CREATE TRIGGER update_ranking AFTER INSERT ON Ranking
+FOR EACH ROW
+BEGIN
+	UPDATE Book
+		SET Calif = (SELECT AVG(GCalif) FROM Ranking WHERE GName = NEW.GName)
+		WHERE Name = NEW.GName;
+END;//
+
+
+CREATE TRIGGER update_ranking AFTER UPDATE ON Ranking
+FOR EACH ROW
+BEGIN
+	UPDATE Book
+		SET Calif = (SELECT AVG(GCalif) FROM Ranking WHERE GName = NEW.GName)
+		WHERE Name = NEW.GName;
+END;//
